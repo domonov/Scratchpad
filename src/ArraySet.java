ebd91c288df6d229d199d01ba2d10ba4681e8300
Davronbek Omonov <domonov@berkeley.edu>
2021-01-19 22:01:20 -0800

    ArraySet is added: iMac

--- src/ArraySet.java
+++ src/ArraySet.java
@@ -1,16 +1,29 @@
 public class ArraySet<Item> {
     private Item[] items;
+    private int last;
 
     public ArraySet() {
-        items = (Item[]) new Object()[];
+        items = (Item[]) new Object[];
+        last = 0;
     }
 
     public void add(Item x) {
-
+        for (Item p : items) {
+            if (p.equals(x)) {
+                return;
+            }
+        }
+        items[last] = x;
+        last += 1;
     }
 
     public boolean contains(Item x) {
-
+        for (Item p : items) {
+            if (p.equals(x)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public int size() {
